23cd3734bc304492a367a8106fd3e669f264c5be
kartikvips <kartikvips@gmail.com>
2018-06-15 13:59:56 -0700

    remove search from body in article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
     include PgSearch
-    multisearchable :against => [:title, :body, :hook]
+    multisearchable :against => [:title, :hook]
 
     validates :title, :body, :user_id, :hook, :img_url, presence: true
     validates :title, uniqueness: true 
